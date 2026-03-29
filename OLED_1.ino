a23f2a9fee4776c84c674ea261a5991d9326e01e
Steve Ernest James <sernest.james@gmail.com>
2026-03-29 09:46:22 -0700

    Add files via upload
    This project uses an OLED with an ESP32.

---
+++ OLED_1.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Display size
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+// Create display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Initialize OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("OLED not found");
+    while (true); // stop here
+  }
+
+  display.clearDisplay();
+
+  // Show welcome text
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 20);
+  display.println("Hello!");
+  display.display();
+
+  delay(2000);
+}
+
+void loop() {
+  showText();
+  drawShapes();
+}
+
+// Function: Display text
+void showText() {
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("OLED Demo");
+
+  display.setTextSize(2);
+  display.setCursor(0, 20);
+  display.println("Welcome");
+
+  display.display();
+  delay(2000);
+}
+
+// Function: Draw basic shapes
+void drawShapes() {
+  display.clearDisplay();
+
+  // Draw rectangle
+  display.drawRect(0, 0, 127, 63, SSD1306_WHITE);
+
+  // Draw line
+  display.drawLine(0, 0, 127, 63, SSD1306_WHITE);
+
+  // Draw circle
+  display.drawCircle(64, 32, 10, SSD1306_WHITE);
+
+  display.display();
+  delay(2000);
+}
